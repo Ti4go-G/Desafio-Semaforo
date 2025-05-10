0d3bdbdc85d45d8d985cb61ef6eb37fa3d3d7ffe
Ti4go-G <tiago.g2511@hotmail.com>
2025-05-10 12:36:14 -0300

    fix: refatorando para inglês

--- Semaforo/js/index.js
+++ Semaforo/js/index.js
@@ -1,72 +1,74 @@
-let tempo;
-let podeSolicitar = true;
-let intervalo;
-let estado = 'verde';
+let time;
+let canRequest = true;
+let interval;
+let state = 'green';
 
-const luzes = {
-    verde: document.querySelector('.verde'),
-    vermelho: document.querySelector('.vermelho'),
-    amarelo: document.querySelector('.amarelo')
+const lights = {
+    green: document.querySelector('.green'),
+    red: document.querySelector('.red'),
+    yellow: document.querySelector('.yellow')
 }
-const button = document.getElementById('botao-pedestre');
+const button = document.getElementById('pedestrian-button');
 const timer = document.getElementById('timer');
 
-function iniciaSemaforo() {
-    tempo = 10;
-    ativarLuz(estado);
-    atualizarIntervalo();
+function startTrafficLight() {
+    time = 10;
+    activateLight(state);
+    refreshInterval();
 }
 
-function ativarLuz(cor) {
-    for (let key in luzes) luzes[key].classList.remove('ativa');
-    luzes[cor].classList.add('ativa');
+function activateLight(color) {
+    for (let key in lights) lights[key].classList.remove('active');
+    lights[color].classList.add('active');
 }
 
-function mudaEstado() {
-    if (estado === 'verde') {
-        estado = 'amarelo';
-        tempo = 3;
-        ativarLuz(estado);
-    } else if (estado === 'amarelo') {
-        estado = 'vermelho';
-        tempo = 15;
-        ativarLuz(estado);
+function changeState() {
+    if (state === 'green') {
+        state = 'yellow';
+        time = 3;
+        activateLight(state);
+    } else if (state === 'yellow') {
+        state = 'red';
+        time = 15;
+        activateLight(state);
     } else {
-        estado = 'verde';
-        tempo = 10;
-        ativarLuz(estado);
+        state = 'green';
+        time = 10;
+        activateLight(state);
     }
 }
 
-function atualizarIntervalo() {
-    clearInterval(intervalo);
-    intervalo = setInterval(() => {
-        tempo--;
-        if (tempo <= 0) mudaEstado();
-        estado === 'amarelo' ? timer.textContent = '' : timer.textContent = tempo;
+function refreshInterval() {
+    clearInterval(interval);
+    interval = setInterval(() => {
+        time--;
+        if (time <= 0) changeState();
+        state === 'yellow' ? timer.textContent = '' : timer.textContent = time;
 
     }, 1000)
 }
 
 button.addEventListener('click', () => {
-    if (!podeSolicitar) return
+    if (!canRequest) return
 
-    podeSolicitar = false;
+    canRequest = false;
     button.disabled = true;
 
 
     setTimeout(() => {
-        podeSolicitar = true;
+        canRequest = true;
         button.disabled = false;
     }, 60000);
 
-    if (estado === 'verde') {
+    if (state === 'green') {
+
         setTimeout(() => {
-            mudaEstado();
+            refreshInterval();
+            changeState();
 
         }, 3000)
     }
 
 })
 
-iniciaSemaforo()
+startTrafficLight()
